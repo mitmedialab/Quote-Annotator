6e2dd04bc3fd084bc5f9e72dadef867552b5c991
Rahul B <rahulbot@gmail.com>
2020-02-18 22:14:10 +0100

    Remove debug comments

--- queue-stories-from-db.py
+++ queue-stories-from-db.py
@@ -1,14 +1,16 @@
 import logging
+import sys
 
 from quoteworker import get_db_client
 from quoteworker.tasks import parse_quotes_to_db
 
 logger = logging.getLogger(__name__)
 
-BATCH_SIZE = 16
+BATCH_SIZE = 1000
 MAX_CHAR_LEN = 90000
 
 collection = get_db_client()
+
 
 # how many left to do?
 total = collection.count_documents({'text': {'$exists': True}})
@@ -18,6 +20,7 @@ logger.info("Stats:")
 logger.info("  {} total".format(total))
 logger.info("  {} have quotes".format(total - unprocessed))
 logger.info("  {} need quotes".format(unprocessed))
+#sys.exit()
 
 # get ​stories with text without quotes from DB
 logger.info("Fetching...")
@@ -31,7 +34,7 @@ for story in collection.find({'annotatedWithQuotes': {'$exists': False}, 'text':
         chunks = [story['text']]
     for c in chunks:
         parse_quotes_to_db.delay({'stories_id': story['stories_id'], 'text': c})
-    logger.info("  queueing up {} ({} chunks)".format(story['stories_id'], len(chunks)))
+    # logger.info("  queueing up {} ({} chunks)".format(story['stories_id'], len(chunks)))
     chunk_count += len(chunks)
     queued += 1
 logger.info("Queued {} stories (in {} < {} char chunks)".format(queued, chunk_count, MAX_CHAR_LEN))
